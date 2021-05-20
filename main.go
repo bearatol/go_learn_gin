01de8cea7da4f54732d48cc730dbab8ed0514019
Igor Khristenko <ikhristenko@avaya.com>
2021-05-20 17:14:15 +0300

    up

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{"title": "Home Page"})
+	})
+
+	router.Run(":8833")
 }
